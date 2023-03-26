7444e9e56f0c156162dce7b834e3807554f78b18
Yuri Mikhin <urasergeevich@mail.ru>
2023-03-26 23:28:14 +0300

    Add authentication and user modules to app.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
