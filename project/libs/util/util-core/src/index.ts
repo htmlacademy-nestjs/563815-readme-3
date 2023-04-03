84b6cf87150a4bfbd1ff00382492e91d7ae88adf
Yuri Mikhin <urasergeevich@mail.ru>
2023-04-03 22:42:25 +0300

    Add getMongoConnectionString.

--- project/libs/util/util-core/src/index.ts
+++ project/libs/util/util-core/src/index.ts
@@ -5,3 +5,7 @@ export function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
     excludeExtraneousValues: true,
   });
 }
+
+export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
