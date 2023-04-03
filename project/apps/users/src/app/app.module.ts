300153d21f7b47fc7dc30d3bfa2112ed62f8dcc8
Yuri Mikhin <urasergeevich@mail.ru>
2023-04-03 20:56:44 +0300

    Add ConfigUsersModule as dependency.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigUsersModule } from "@project/config/config-users";
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [UsersModule, AuthenticationModule, ConfigUsersModule],
   controllers: [],
   providers: [],
 })
