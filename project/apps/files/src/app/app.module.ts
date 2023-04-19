fe17f19d0d89a93089c4f464b28f9dcb1966f96b
Yuri Mikhin <urasergeevich@mail.ru>
2023-04-19 19:18:29 +0300

    Add db to files app.

--- project/apps/files/src/app/app.module.ts
+++ project/apps/files/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigFilesModule } from '@project/config/config-files';
+import {
+  ConfigFilesModule,
+  getMongooseOptions,
+} from '@project/config/config-files';
 import { FileModule } from './file/file.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigFilesModule, FileModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    FileModule,
+    ConfigFilesModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- project/libs/config/config-files/src/lib/files.config.ts
+++ project/libs/config/config-files/src/lib/files.config.ts
@@ -2,11 +2,20 @@ import * as Joi from 'joi';
 import { registerAs } from '@nestjs/config';
 
 const DEFAULT_PORT = 3000;
+const DEFAULT_MONGO_PORT = 27018;
 
 export interface FilesConfig {
   environment?: string;
   uploadDirectory?: string;
   port?: number;
+  db: {
+    host?: string;
+    port?: number;
+    user?: string;
+    name?: string;
+    password?: string;
+    authBase?: string;
+  };
 }
 
 export default registerAs('application', (): FilesConfig => {
@@ -14,12 +23,31 @@ export default registerAs('application', (): FilesConfig => {
     environment: process.env.NODE_ENV,
     uploadDirectory: process.env.UPLOAD_DIRECTORY_PATH,
     port: parseInt(process.env.POR || DEFAULT_PORT.toString(), 10),
+    db: {
+      host: process.env.MONGO_HOST,
+      port: parseInt(
+        process.env.MONGO_PORT ?? DEFAULT_MONGO_PORT.toString(),
+        10
+      ),
+      name: process.env.MONGO_DB,
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASSWORD,
+      authBase: process.env.MONGO_AUTH_BASE,
+    },
   };
 
   const validationSchema = Joi.object<FilesConfig>({
     environment: Joi.string().valid('development', 'production', 'stage'),
     port: Joi.number().port().default(DEFAULT_PORT),
     uploadDirectory: Joi.string(),
+    db: Joi.object({
+      host: Joi.string().valid().hostname(),
+      port: Joi.number().port(),
+      name: Joi.string().required(),
+      user: Joi.string().required(),
+      password: Joi.string().required(),
+      authBase: Joi.string().required(),
+    }),
   });
 
   const { error } = validationSchema.validate(config, { abortEarly: true });
