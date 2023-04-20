26c05f973a545a12e4f377127d5d8b7e162fd7f7
Yuri Mikhin <urasergeevich@mail.ru>
2023-04-20 12:48:03 +0300

    Add JwtModule to Auth module.

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -1,10 +1,19 @@
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
+import { getJwtOptions } from '@project/config/config-users';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    }),
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
